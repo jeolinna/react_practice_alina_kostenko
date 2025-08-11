56cfa71a4262397e5dbeca3ed986ca193a59465d
alina <kostenkoalina97@icloud.com>
2025-08-11 11:16:20 +0200

    add class for AllCategories button

--- src/App.jsx
+++ src/App.jsx
@@ -114,7 +114,9 @@ export const App = () => {
               <a
                 href="#/"
                 data-cy="AllCategories"
-                className="button is-success mr-6 is-outlined"
+                className={cn('button is-success mr-6', {
+                  'is-outlined': filteredByCategory.length !== 0,
+                })}
                 onClick={() => setFilteredByCategory([])}
               >
                 All
